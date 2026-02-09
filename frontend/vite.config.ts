fd74b19959f764d33851028e7c92004f65f57f3a
Celixwk <jccpp45@gmail.com>
2026-02-09 15:50:48 -0500

    chore: functional program 2.0 update - dependency cleanup and config tweaks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
